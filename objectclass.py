209a8bc0529c6f32dc526e901074d396a6657336
Phillip Post <phillip.post@gmail.com>
2022-12-08 12:09:55 -0500

    initial object detection framework implemented

--- objectclass.py
+++ objectclass.py
@@ -12,7 +12,7 @@ class objectdetect():
         # by opencv when fed into objectdetect
         self.image = image
         # create a placeholder for the primary color of the object
-        # note color is a list of floats in BGR format
+        # note color is a list of floats in HSV format
         self.primary_color = None
         # create a placeholder for the corners of the object
         self.diameter = diameter
@@ -20,7 +20,7 @@ class objectdetect():
         self.name = name
 
     def find_color(self):
-        # convert the image to a data matrix pixels * RGB
+        # convert the image to a data matrix pixels * HSV
         image_matrix = self.image.reshape(self.image.shape[0]*self.image.shape[1], 3)
 
         # find the clusters
@@ -29,7 +29,7 @@ class objectdetect():
         labels = clusters.fit_predict(image_matrix)
 
         # count which cluster is the most dominant
-        label_count = st.mode(labels)
+        label_count = st.mode(labels, keepdims = True)
 
         # find the most popular color (centroid)
         dominant_color = clusters.cluster_centers_[label_count.mode[0]]
--- objecttrack.py
+++ objecttrack.py
@@ -16,10 +16,10 @@ import re
 
 class objecttrack():
     def __init__(self, folder_name):
-        # create a placeholder to store the tracking objects
-        self.objects = None
         # store the name of the folder where the object images are stored
         self.folder_name = folder_name
+        # create a placeholder to store the tracking objects
+        self.objects = None
 
     def load_objects(self):
 
@@ -39,8 +39,8 @@ class objecttrack():
             except NameError:
                 print("Object Image Name has no Diameter ending, proper ex. greenball_5")
 
-            # load the image
-            img = cv.imread(path + "/" + objects)
+            # load the BGR image
+            img = cv.imread(path + "/" + objects, cv.COLOR_BGR2HSV)
 
             # create an instance of the image
             landmark = objectdetect(img,diameter,objects)
@@ -56,10 +56,79 @@ class objecttrack():
         # Move the objects to a global placeholder
         self.objects = object_list
 
+    def findcircles(self, image):
+        # Apply a median blur to the image to reduce noise
+        image = cv.medianBlur(image, 5)
 
-test = objecttrack("trackingobjects")
+        # find all of the circles in the image frame
+        circles = cv.HoughCircles(image, cv.HOUGH_GRADIENT, 1, 20)
 
-test.load_objects()
+        # circles is a list of circle objects
+        return circles
 
-print(test.objects[0].name)
-print(test.objects[0].primary_color)
+    def matchobjects(self, image, circles, tolerance):
+        # identify if any of the circles are a known object in the user defined folder
+
+        for circle in circles:
+            # find if the center color of the circle matches any of the colors
+            # note that images are [y,x,c] format
+            circle_color = image[circle[1], circle[0]]
+
+            # now assess if this color is within the tolerance range of any of the object colors
+            for obj in self.objects:
+                lower_range = [value * (1 - (tolerance/100)) for value in obj.primary_color]
+                upper_range = [value * (1 - (tolerance/100)) for value in obj.primary_color]
+
+                # find if the circle color is in the range of the color
+                # zip creates a list of tuples
+                lower_test = [True if x > y else False for x,y in zip(circle_color, lower_range)]
+                upper_test = [True if x < y else False for x,y in zip(circle_color, upper_range)]
+
+                # if any colors fall out of the range, move on
+                if False in lower_test or False in upper_test:
+                    continue
+                else:
+                    print(f"Matched object {obj.name}")
+
+
+
+if __name__ == '__main__':
+
+    tracker = objecttrack("trackingobjects")
+    tracker.load_objects()
+
+    # define where to receive the video stream
+    cap = cv.VideoCapture(0)
+
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+
+    while True:
+        # load the live video feed
+        ret, frame = cap.read()
+        if frame is None:
+            continue
+
+        # convert the video frame to HSV
+        live_image = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        # also convert it to grey scale for Hough transformations
+        grey_image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # find the circles
+        circles = tracker.findcircles(grey_image)
+
+        if circles is not None and len(circles) > 0:
+            print("circle found!")
+            # match the circles to objects
+            tracker.matchobjects(live_image, circles, 5)
+
+
+        # set the dimensions of the video frame
+        frame = np.array(cv.resize(frame,
+                                    (frame.shape[1]//1,
+                                     frame.shape[0]//1)))
+        cv.imshow('Video Feed', frame)
+        # if the user presses esc, terminate the process
+        cv.waitKey(27)
+    cv.destroyAllWindows()
