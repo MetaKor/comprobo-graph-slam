84ebb6446674e4668d8ccc2cffb717dd20fc9dc2
Florian Schwarzinger <firestarss88@gmail.com>
2022-12-18 22:09:37 -0500

    add assets folder and fix test_graphslam

--- neato_graphslam/test_graphslam.py
+++ neato_graphslam/test_graphslam.py
@@ -7,6 +7,9 @@ from graphslam.edge.edge_odometry import EdgeOdometry
 
 def main():
 
+    odometry_info_matrix = np.array([[2,0,0], [0,2,0], [0,0,1]])
+    landmark_info_matrix = np.array([[1,0,0], [0,1,0], [0,0,0]])
+
     # Make PoseSE2 objects representing robot's "true" poses
     # ang_d represents angle along unit circle on which to place robot
     # Robot's heading is +90 degrees offset from this angle (at bottom, pointing right)
@@ -16,7 +19,7 @@ def main():
                  for ang_d in range(-90, 90+1, 20) ]
 
     # Give poses additive Gaussian noise
-    noisyPoses = [ truePose + PoseSE2(np.random.normal(scale=0.06, size=2), \
+    noisyPoses = [ truePose + PoseSE2(np.random.normal(scale=0.5, size=2), \
                                       np.random.normal(scale=0.03) ) \
                  for truePose in truePoses ]
 
@@ -32,8 +35,8 @@ def main():
 
     # Creates edges between all robot poses
     odom_edges = [ EdgeOdometry( [i, i+1], \
-                                 np.eye(3), \
-                                 noisyPoses[i+1] - noisyPoses[i] + \
+                                 odometry_info_matrix, \
+                                 truePoses[i+1] - truePoses[i] + \
                                      PoseSE2(np.random.normal(scale=0.05, size=2), np.random.normal(scale=0.02)), #estimate
                                  [vertices[i], vertices[i+1]]) \
                   for i in range( n_poses-1 ) ]
@@ -41,15 +44,15 @@ def main():
     # Landmark edge information matrices have a zero on 3rd diagonal element
     # to signify that orientation/angle of landmarks is irrelevant
     landmark_edges = [ EdgeOdometry( [n_poses, i],
-                                     np.array([[1,0,0], [0,1,0], [0,0,0]]),
-                                     -noisyPoses[i] + PoseSE2(np.random.normal(scale=0.05, size=2), 0), \
+                                     landmark_info_matrix,
+                                     -truePoses[i] + PoseSE2(np.random.normal(scale=0.05, size=2), 0), \
                                      [vertices[n_poses], vertices[i]] ) \
                       for i in range( n_poses ) ]
 
     edges = odom_edges + landmark_edges
 
-    print(vertices)
-    print(edges)
+    # print(vertices)
+    # print(edges)
 
     g = Graph(edges, vertices)
 
